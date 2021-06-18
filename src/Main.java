00366d69ce0588c8ed5a1e9b5c9c02fab02eb9d8
Tatyana Fomina <tatyana24fomina@gmail.com>
2021-06-18 04:44:18 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int payment = 500_000_000;
+        int total = balance + payment;
+        System.out.println(total);
+    }
+}
